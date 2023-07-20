246896e7a4a6d52310de92f6b14cbeac14e58294
Enzuii <enzuii@hotmail.com>
2023-07-20 17:26:21 -0300

    Documentos de arquitectura

--- ApiNodeJSExpress/src/config/database.js
+++ ApiNodeJSExpress/src/config/database.js
@@ -2,7 +2,7 @@
 const { Sequelize } = require('sequelize');
 
 // Configura los datos de conexión a la base de datos PostgreSQL
-const sequelize = new Sequelize('postgres', 'postgres', 'admin', {
+const sequelize = new Sequelize('postgres', 'postgres', 'enzotricolor3', {
   host: 'localhost', // Cambia esto si tu base de datos está en un servidor remoto
   dialect: 'postgres', // Indica que estamos utilizando PostgreSQL
   logging: false, // Desactiva los logs de Sequelize
--- ApiNodeJSExpress/src/models/userModel.js
+++ ApiNodeJSExpress/src/models/userModel.js
@@ -1,12 +1,16 @@
 const { DataTypes } = require('sequelize');
 const db = require('../config/database');
 
-const UserModel = db.define('Usuario', {
+const UserModel = db.define('usuario', {
   // Definición de los campos del modelo de usuario
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
   }
   // Otros campos del usuario (puedes agregar más según tus necesidades)
 });
