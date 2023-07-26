4dc14b2e8794382385c3dfdf3554652a85a27c5c
Enzuii <enzuii@hotmail.com>
2023-07-26 15:29:50 -0300

    Faltó Model de Usuario

---
+++ ApiNodeJSExpress/src/models/userModel.js
@@ -0,0 +1,45 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database.js');
+
+class UserModel extends Model {}
+
+UserModel.init(
+  {
+    // Propiedades del usuario
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+    },
+    apellido: {
+      type: DataTypes.STRING,
+    },
+    ci: {
+      type: DataTypes.INTEGER,
+    },
+    telefono: {
+      type: DataTypes.STRING,
+    },
+    ciudad:{
+      type: DataTypes.STRING,
+    },
+    pais: {
+      type: DataTypes.STRING,
+    },
+    departamento: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Usuarios', // Nombre del modelo
+    tableName: 'usuarios', // Nombre de la tabla en la base de datos
+  }
+);
+
+
+
+module.exports = UserModel;
