6e6d1666b4c1b74543363116016cda4518cdbfe5
Giorgio <Giorgio.san007@gmail.com>
2025-04-03 03:52:04 -0300

    improve in docs

--- ApiNodeJSExpress/src/index.js
+++ ApiNodeJSExpress/src/index.js
@@ -38,7 +38,7 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
  
  
  
