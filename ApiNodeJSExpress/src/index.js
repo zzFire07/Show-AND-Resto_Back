a633eceb4b915f4c2f84548c0096810d1e45eef7
Giorgio <Giorgio.san007@gmail.com>
2025-04-03 01:00:56 -0300

    minimun fix

--- ApiNodeJSExpress/src/index.js
+++ ApiNodeJSExpress/src/index.js
@@ -16,8 +16,7 @@ app.use(express.json());
  
 
 //Routes
-app.use(require('./routes/restaurantRoutes.js')); 
-app.use(require('./routes/userRoutes.js')); 
+app.use(require('./routes/restaurantRoutes.js'));
 app.use(require('./routes/showRoutes.js')); 
 app.use(require('./routes/foodTypeRoutes.js'));
 
