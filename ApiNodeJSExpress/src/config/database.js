3ae697b9f8155f0be390a9bc4d576d9fff2618fd
Giorgio <Giorgio.san007@gmail.com>
2025-04-09 16:00:04 -0300

    debugging 3

--- ApiNodeJSExpress/src/config/database.js
+++ ApiNodeJSExpress/src/config/database.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 const DATABASE_URL = "postgresql://sardb_owner:npg_CUK6cw9yezVS@ep-spring-truth-a5zp7cb9-pooler.us-east-2.aws.neon.tech/sardb?sslmode=require";
 
 // Configura los datos de conexión a la base de datos PostgreSQL
-const sequelize = new Sequelize(DATABASE_URL, {});
+const sequelize = new Sequelize(DATABASE_URL, {logging: console.log,});
 
 // Realiza la conexión a la base de datos
 (async () => {
